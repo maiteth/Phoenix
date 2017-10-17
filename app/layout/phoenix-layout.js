22890861f2bc1e5bf1dd418b91bdaa431a571505
Maïté THOMIAS <maite.thomias@gmail.com>
2017-10-17 13:53:19 +0200

    add: phoenix body

--- app/layout/phoenix-layout.js
+++ app/layout/phoenix-layout.js
@@ -5,3 +5,7 @@ const app = angular.module('phoenix-layout', []);
 app.component('phoenixHeader', {
     templateUrl: './layout/header/phoenix-header.html'
 });
+
+app.component('phoenixBody', {
+    templateUrl: './layout/body/phoenix-body.html'
+});
